78afb10ab4f221b9cc40d016678cd1f1cd82bc20
ashLuchowa <ashwinluchowa@gmail.com>
2024-05-19 12:37:36 +0800

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,20 @@ module.exports = {
             template: 'src/index.html',
         }),
     ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module : {
         rules: [
