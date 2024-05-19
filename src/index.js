ee712c6326a7bcae46d2c837c80006c6a130836e
ashLuchowa <ashwinluchowa@gmail.com>
2024-05-19 14:22:33 +0800

    Implement menu tab browsing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import './style.scss';
 import initialPage from './initialPage';
 
 function component() {
+    const buttons = document.querySelectorAll('.main-nav button');
+
+    // Clicking menu tab
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            if(e.target.textContent === 'Home') {
+                if(!initialPage) {
+                    initialPage();
+                }
+            }
+        }, {once:true});
+    });
+
+    // Display initial page
     initialPage();
 }
 
