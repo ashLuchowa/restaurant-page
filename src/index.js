41d10caf3fe9735a8a95a9fe10f67a04d6ee42c5
ashLuchowa <ashwinluchowa@gmail.com>
2024-05-19 11:15:12 +0800

    Added image loader + CSS loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+import './style.css';
+import img1 from './img1.jpg';
+
+const mainContent = document.getElementById('content');
+
+const myImage = new Image();
+myImage.src = img1;
+
+mainContent.appendChild(myImage);
+
+console.log('hi');
